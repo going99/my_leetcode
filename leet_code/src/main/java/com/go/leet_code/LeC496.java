6ed2b5e43967b48e798481cd2c946bb3c6a0b943
jago <jago99@163.com>
2021-12-01 15:44:04 +0800

    #leetcode

--- leet_code/src/main/java/com/go/leet_code/LeC496.java
+++ leet_code/src/main/java/com/go/leet_code/LeC496.java
@@ -1,10 +1,10 @@
 package com.go.leet_code;
 
 /**
- * Description: (下一个更大元素)
+ * Description: (下一个更大元素)-简单
  * 给定两个没有重复元素的数组 nums1与 nums2,其中 nums1 是 nums2 的子集。找到 nums1 中每个元素在 nums2 中的下一个比其大的值。
  * nums1 中的数字x的下一个更大元素是指 x 的 nums2 中对应位置的右边的第一个比 x 大的元素。如果不存在，对应位置输出-1
- *
+ * <p>
  * 示例1：
  * 输入nums1=[4,1,2],nums2=[1,3,4,2]
  * 输出 [-1,3,-1]
@@ -17,10 +17,32 @@ package com.go.leet_code;
  */
 public class LeC496 {
 
-    public static void main(String[] args) {
-
+    private static void printArr(int[] arr) {
+        if (null == arr) {
+            return;
+        }
+        for (int i : arr) {
+            System.out.println(" " + i);
+        }
     }
 
+    public static void main(String[] args) {
+        int[] nums1 = new int[]{4, 1, 2};
+        int[] nums2 = new int[]{1, 3, 4, 2};
+        int[] element = nextMoreGreaterElement(nums1, nums2);
+        printArr(element);
+    }
 
+    /**
+     * 生成下一个更大的元素
+     *
+     * @param nums1
+     * @param nums2
+     * @return
+     */
+    private static int[] nextMoreGreaterElement(int[] nums1, int[] nums2) {
+
+        return null;
+    }
 
 }
