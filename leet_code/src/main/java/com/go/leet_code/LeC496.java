cc17fa051b3271be36ea1a9c57ab4dfa2dc1fdc6
jago <jago99@163.com>
2021-12-01 15:19:16 +0800

    #新建项目

---
+++ leet_code/src/main/java/com/go/leet_code/LeC496.java
@@ -0,0 +1,26 @@
+package com.go.leet_code;
+
+/**
+ * Description: (下一个更大元素)
+ * 给定两个没有重复元素的数组 nums1与 nums2,其中 nums1 是 nums2 的子集。找到 nums1 中每个元素在 nums2 中的下一个比其大的值。
+ * nums1 中的数字x的下一个更大元素是指 x 的 nums2 中对应位置的右边的第一个比 x 大的元素。如果不存在，对应位置输出-1
+ *
+ * 示例1：
+ * 输入nums1=[4,1,2],nums2=[1,3,4,2]
+ * 输出 [-1,3,-1]
+ * 解释：对于nums1 中的那数字4，你无法在第二个数组中找到下一个更大的数字，因此输出-1；
+ * 对于nums1中的数字1，第二个数组中数字1右边的下一个较大的数字是3;
+ * 对于nums1中的数字2，第二个数组中没有下一个更大的数字，因此输出-1.
+ * Created on 2021/12/1.
+ *
+ * @author go
+ */
+public class LeC496 {
+
+    public static void main(String[] args) {
+
+    }
+
+
+
+}
---
+++ leet_code/src/main/java/com/go/leet_code/LeC739.java
@@ -0,0 +1,32 @@
+package com.go.leet_code;
+
+import java.util.Stack;
+
+/**
+ * Description: 739 请根据每日气温列表，重新生成一个列表，对应位置的输出为：要想观测到更高的气温，至少需要等待的天数，如果气温在这之后都不会升高，则在该位置用0来位置
+ * Created on 2021/12/1.
+ * 单调栈算法
+ *
+ * @author go
+ */
+public class LeC739 {
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{73,74,75,71,69,72,76,73};
+        int[] sort = sort(arr);
+        System.out.println("sort = " + sort);
+    }
+
+    private static int[] sort(int[] arr) {
+        int[] res = new int[arr.length];
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < arr.length; i++) {
+            while (!stack.isEmpty() && arr[stack.peek()] < arr[i]) {
+                res[stack.peek()] = i - stack.pop();
+            }
+            stack.push(i);
+        }
+        return res;
+    }
+
+}
